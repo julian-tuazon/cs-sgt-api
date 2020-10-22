a5b0498b764a396b34587941dea9cae0d9b00649
Julian <jthrow00@gmail.com>
2020-10-22 01:39:20 -0700

    Modified data model class to include course and grade

--- SgtApi/Models/TodoItem.cs
+++ SgtApi/Models/TodoItem.cs
@@ -8,7 +8,15 @@ namespace SgtApi.Models
     public class TodoItem
     {
         public long Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
-        public bool IsComplete { get; set; }
+
+        [Required]
+        public string Course { get; set; }
+
+        [Required]
+        [Range(minimum: 0.01, maximum: (double) decimal.MaxValue)]
+        public int Grade { get; set; }
     }
 }
