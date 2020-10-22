57901905fce70518ec0187aade5b85d894e5ae54
Julian <jthrow00@gmail.com>
2020-10-22 02:59:08 -0700

    Changed Model Grade class to have string CurrentGrade; renamed file

---
+++ SgtApi/Models/Grade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SgtApi.Models
+{
+    public class Grade
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Course { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int CurrentGrade { get; set; }
+    }
+}
