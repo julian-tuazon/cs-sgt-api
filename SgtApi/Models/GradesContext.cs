983f752e4af2b8f1fbc144fb2e083dfbcf63fe95
Julian <jthrow00@gmail.com>
2020-10-22 02:59:36 -0700

    Renamed Context class and members

---
+++ SgtApi/Models/GradesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SgtApi.Models
+{
+    public class GradesContext : DbContext
+    {
+        public GradesContext(DbContextOptions<GradesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
